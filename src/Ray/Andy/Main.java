17a931bb482c72c547c32b0c932e7f1d5fa91c45
Kov4lnk <kov4lnk@gmail.com>
2021-01-20 12:02:43 +0200

    Initial commit

---
+++ src/Ray/Andy/Main.java
@@ -0,0 +1,36 @@
+package Ray.Andy;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+
+        switch (myChar){
+            case 'A':
+                System.out.println("It is " + myChar);
+                break;
+
+            case 'B':
+                System.out.println("It is " + myChar);
+                break;
+
+            case 'C':
+                System.out.println("It is " + myChar);
+                break;
+
+            case 'D':
+                System.out.println("It is " + myChar);
+                break;
+
+            case 'E':
+                System.out.println("It is " + myChar);
+                break;
+
+            default:
+                System.out.println("not found");
+                break;
+
+        }
+    }
+}
